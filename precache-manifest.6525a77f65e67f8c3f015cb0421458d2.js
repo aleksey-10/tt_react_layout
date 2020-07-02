2b114667434a979397334ac214e6c554f9cf12b3
aleksey-10 <alexcare1337@gmail.com>
2020-07-02 13:49:07 +0300

    deploy

---
+++ precache-manifest.6525a77f65e67f8c3f015cb0421458d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fa917084f8ce4844d1c3e78410ef37",
+    "url": "/tt_react_layout/index.html"
+  },
+  {
+    "revision": "76819aa28a5fdcddd3a5",
+    "url": "/tt_react_layout/static/css/main.d867ad95.chunk.css"
+  },
+  {
+    "revision": "7df61b93d731a9a16004",
+    "url": "/tt_react_layout/static/js/2.98862853.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/tt_react_layout/static/js/2.98862853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76819aa28a5fdcddd3a5",
+    "url": "/tt_react_layout/static/js/main.0fb297f7.chunk.js"
+  },
+  {
+    "revision": "9435f914f47c49e9fe7a",
+    "url": "/tt_react_layout/static/js/runtime-main.69117831.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/tt_react_layout/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/tt_react_layout/static/media/burger.6f6bef28.svg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/tt_react_layout/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/tt_react_layout/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
